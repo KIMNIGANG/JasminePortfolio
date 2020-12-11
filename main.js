73db725051d07eed6996bc0d78ebe1497b42b580
kimnigang <zeus2424797@gmail.com>
2020-12-11 18:03:20 +0900

    CSS

--- main.js
+++ main.js
@@ -117,3 +117,28 @@ workBtn.addEventListener("click", (event) => {
     projectContainer.classList.remove("anim-out");
   }, 300);
 });
+
+//navbar active scroll
+(function () {
+  "use strict";
+
+  var section = document.querySelectorAll("section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function (e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function () {
+    var scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector(".active").setAttribute("class", " ");
+        document.querySelector(".navbar_item").setAttribute("class", "active");
+      }
+    }
+  };
+})();
